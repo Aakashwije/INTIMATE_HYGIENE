7dc4550f397d31ca6d6ee47d4863c6eef5dc01de
Aakashiit <aakashwije92@gmail.com>
2025-09-19 03:09:47 +0530

    last update

--- Global /script.js
+++ Global /script.js
@@ -4,3 +4,11 @@ const navLinks = document.getElementById('navLinks');
 hamburger.addEventListener('click', () => {
   navLinks.classList.toggle('show'); // toggles the display
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+  const goTopBtn = document.getElementById("goTopBtn");
+
+  goTopBtn.addEventListener("click", function() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  });
+});
